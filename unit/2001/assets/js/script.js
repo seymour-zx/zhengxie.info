50237e0b33d409d9b2be1a77f0b9780011872d9b
seymour-zx <seymour.zx@foxmail.com>
2024-09-28 01:52:24 +0800

    666

---
+++ unit/2001/assets/js/script.js
@@ -0,0 +1,13 @@
+const navBar = document.querySelector("header"),
+       menuBtns = document.querySelectorAll(".button-menu"),
+       overlay = document.querySelector(".overlay");
+
+     menuBtns.forEach((menuBtn) => {
+       menuBtn.addEventListener("click", () => {
+         navBar.classList.toggle("open");
+       });
+     });
+
+     overlay.addEventListener("click", () => {
+       navBar.classList.remove("open");
+     });
